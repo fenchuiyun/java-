a9d52c5724bedd65cd6eba969428ceafa56967ea
fenchuiyun <1244163310@qq.com>
2019-10-13 16:03:50 +0800

    多线程控制模型

---
+++ JUC/src/main/java/com/fenchuiyun/juc/ThreadOrderAccess.java
@@ -0,0 +1,106 @@
+package com.fenchuiyun.juc;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class ShareResource{
+    private  int flag =1;// 1:A 2:B 3:C
+    private Lock lock = new ReentrantLock();
+    private Condition c1 = lock.newCondition();
+    private Condition c2 = lock.newCondition();
+    private Condition c3 = lock.newCondition();
+
+    public void print5() {
+        lock.lock();
+        try {
+            //判断
+            while (flag != 1) {
+                //A系统就要停
+                c1.await();
+            }
+            //2 干活
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            //3 通知
+            flag = 2;
+            c2.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print10(){
+        lock.lock();
+        try{
+            //判断
+            while (flag!=2){
+                //B系统就要停
+            }
+            //2 干活
+            for (int i = 0; i <10 ; i++) {
+                System.out.println(Thread.currentThread().getName()+"\t"+i);
+            }
+            //3 通知
+            flag=3;
+            c3.signal();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            lock.unlock();
+        }
+    }
+
+    public void print15(){
+        lock.lock();
+        try{
+            //判断
+            while (flag!=3){
+                //C系统就要停
+            }
+            //2 干活
+            for (int i = 0; i <15 ; i++) {
+                System.out.println(Thread.currentThread().getName()+"\t"+i);
+            }
+            //3 通知
+            flag=1;
+            c1.signal();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            lock.unlock();
+        }
+    }
+}
+/**
+ * Created by 12441 on 2019/10/13
+ * 多线程之间按顺序调用，实现A->B->C
+ * 三个线程启动，要求如下:
+ *
+ * AA打印5次，BB打印10次，CC打印15次
+ * 接着
+ * AA打印5次，BB打印10次，CC打印15次
+ * 。。。来个10次
+ *
+ * 1    高聚合低耦合前提下，线程操作资源类
+ * 2    判断/干活/通知
+ * 3    多线程交互中，必须防止对现场的虚假唤醒，用while不用if
+ * 4    注意判断标志位的更新
+ */
+public class ThreadOrderAccess {
+    public static void main(String[] args) {
+        ShareResource shareResource = new ShareResource();
+        new Thread(()->{
+            shareResource.print5();
+        },"input thread name").start();
+        new Thread(()->{
+            shareResource.print10();
+        },"input thread name").start();
+        new Thread(()->{
+            shareResource.print15();
+        },"input thread name").start();
+    }
+}
--- JUC/src/main/java/com/fenchuiyun/juc/ThreadWaitNotifyDemo.java
+++ JUC/src/main/java/com/fenchuiyun/juc/ThreadWaitNotifyDemo.java
@@ -1,13 +1,17 @@
 package com.fenchuiyun.juc;
 
 import java.util.Objects;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Count{
     private int count =0;
-    
-    public synchronized void incrementBy1(){
+    private Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+/*    public synchronized void incrementBy1(){
         //1 判断
-        if(count!=0){//防止虚假唤醒，要用while进行判断
+        while (count!=0){//防止虚假唤醒，要用while进行判断
             try {
 
                 this.wait();
@@ -23,7 +27,7 @@ class Count{
     }
     
     public synchronized void decrementBy1(){
-        if (count==0){
+        while (count==0){
             try {
                 this.wait();
             } catch (InterruptedException e) {
@@ -33,6 +37,49 @@ class Count{
         count--;
         System.out.println(count);
         this.notifyAll();
+    }*/
+    public void incrementBy1() {
+        lock.lock();
+        try{
+            while (count!=0){//防止虚假唤醒，要用while进行判断
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            //2 干活
+            count++;
+            System.out.println(count);
+            //3 通知
+            condition.signalAll();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            lock.unlock();
+        }
+    }
+
+    public synchronized void decrementBy1(){
+        lock.lock();
+        try{
+            while (count==0){//防止虚假唤醒，要用while进行判断
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            //2 干活
+            count--;
+            System.out.println(count);
+            //3 通知
+            condition.signalAll();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            lock.unlock();
+        }
     }
 }
 /**
@@ -43,7 +90,7 @@ class Count{
  *lock和synchronized有啥区别??
  * 1    高内聚低耦合的前提下，线程操作资源类
  * 2    判断/干活/通知
- * 3
+ * 3    小心，防止多线程中的虚假唤醒,判断时候用while而不是if
  */
 public class ThreadWaitNotifyDemo {
     public static void main(String[] args) throws Exception{
@@ -52,12 +99,24 @@ public class ThreadWaitNotifyDemo {
             for (int i = 0; i <10 ; i++) {
                 count.incrementBy1();
             }
-        },"input thread name").start();
+        },"A").start();
         
         new Thread(()->{
             for (int i = 0; i <10 ; i++) {
                 count.decrementBy1();
             }
-        },"input thread name").start();
+        },"B").start();
+
+        new Thread(()->{
+            for (int i = 0; i <10 ; i++) {
+                count.incrementBy1();
+            }
+        },"C").start();
+
+        new Thread(()->{
+            for (int i = 0; i <10 ; i++) {
+                count.decrementBy1();
+            }
+        },"D").start();
     }
 }
